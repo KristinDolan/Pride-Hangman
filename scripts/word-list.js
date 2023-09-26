184b280c43322385b18437add680cf5c2cfaa461
Kristin Hill <krstinhill4211@gmail.com>
2023-09-25 21:53:58 -0600

    working!

---
+++ scripts/word-list.js
@@ -0,0 +1,50 @@
+const wordList = [
+    {
+        word: "piano",
+        hint: "I'm black and white and all strung up."
+    },
+    {
+        word: "molecule",
+        hint: "I'm too small for the eye to see, but you'd be nothing without me."
+    },
+    {
+        word: "peanut butter",
+        hint: "Smooth and sweet, I'm nice to eat."
+    },
+    {
+        word: "basketball",
+        hint: "I'm the color and shape of a fruit, but I probably taste more like a boot."
+    },
+    {
+        word: "gallows",
+        hint: "You'd better guess me, or this is where you'll be!"
+    },
+    {
+        word: "elephant",
+        hint: "I make a sound like triumphant brass, but I'd rather eat some grass."
+    },
+    {
+        word: "heaven",
+        hint: "You'll only get here if you're good."
+    },
+    {
+        word: "wool",
+        hint: "Socks or sweaters are my best use, but idk some pun"
+    },
+    {
+        word: "window",
+        hint: "When you look at me, what's beyond is what you see."
+    },
+    {
+        word: "staircase",
+        hint: "I'll take you to another place, but get it wrong and I might break your face."
+    },
+    {
+        word: "jazz",
+        hint: "I'll make you dance if you give me the chance."
+    },
+    {
+        word: "sunrise",
+        hint: "To see me you might need a coffee, and though I'm bright I begin softly."
+    }
+]
